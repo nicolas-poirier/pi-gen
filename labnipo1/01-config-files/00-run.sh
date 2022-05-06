7537b2b9bc99eabc0f757847930b5f12326c7819
Nicolas Poirier <nicolas2.poirier@laposte.net>
2022-05-06 17:04:21 +0200

    Update 00-run.sh

--- labnipo1/01-config-files/00-run.sh
+++ labnipo1/01-config-files/00-run.sh
@@ -2,3 +2,4 @@
 
 copy_overlay etc/network/interfaces -o root -g root -m 644
 copy_overlay /etc/wpa_supplicant/wpa_supplicant.conf -o root -g root -m 600
+copy_overlay /etc/sudoers.d/wlancapture -o root -g root -m 600
