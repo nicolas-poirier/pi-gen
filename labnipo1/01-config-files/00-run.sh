28ce82c62f7c91ee84c05573a5fae45b3a4a1792
Nicolas Poirier <nicolas2.poirier@laposte.net>
2022-05-11 08:51:50 +0200

    Update 00-run.sh

--- labnipo1/01-config-files/00-run.sh
+++ labnipo1/01-config-files/00-run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -e
 
-#copy_overlay etc/network/interfaces -o root -g root -m 644
-#copy_overlay /etc/wpa_supplicant/wpa_supplicant.conf -o root -g root -m 600
-#copy_overlay /etc/sudoers.d/wlancapture -o root -g root -m 600
+copy_overlay /etc/dhcpcd.conf -o root -g root -m 600
+copy_overlay /etc/dnsmasq.conf -o root -g root -m 600
+copy_overlay /etc/sysctl.conf -o root -g root -m 600
+copy_overlay /etc/sudoers.d/labnipo -o root -g root -m 600
+copy_overlay /etc/systemd/system/autohotspot.service -o root -g root -m 600
+copy_overlay /etc/wpa_supplicant/wpa_supplicant.conf -o root -g root -m 600
+copy_overlay /usr/bin/autohotspotN -o root -g root -m 600
+
+# hostapd and dnsmasq will be controlled by the autohotspot service
+systemctl unmask hostapd
+systemctl disable hostapd
+systemctl disable dnsmasq
+
+chmod +x /usr/bin/autohotspotN
+systemctl enable autohotspot.service
