52d940deb171395e5c5a3b380d85f551b78166f4
Nicolas Poirier <nicolas2.poirier@laposte.net>
2022-05-11 16:58:03 +0200

    Update 00-run.sh

--- labnipo1/01-config-files/00-run.sh
+++ labnipo1/01-config-files/00-run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -e
 
-copy_overlay /etc/dhcpcd.conf -o root -g root -m 600
-copy_overlay /etc/dnsmasq.conf -o root -g root -m 600
-copy_overlay /etc/sysctl.conf -o root -g root -m 600
-copy_overlay /etc/sudoers.d/labnipo -o root -g root -m 600
-copy_overlay /etc/systemd/system/autohotspot.service -o root -g root -m 600
-copy_overlay /etc/wpa_supplicant/wpa_supplicant.conf -o root -g root -m 600
-copy_overlay /usr/bin/autohotspotN -o root -g root -m 600
+install -m 644 files/etc/dhcpcd.conf ${ROOTFS_DIR}/etc/dhcpcd.conf
+install -m 644 files/etc/dnsmasq.conf ${ROOTFS_DIR}/etc/dnsmasq.conf
+install -m 644 files/etc/sysctl.conf ${ROOTFS_DIR}/etc/sysctl.conf
+install -m 644 files/etc/sudoers.d/labnipo ${ROOTFS_DIR}/etc/sudoers.d/labnipo
+install -m 644 files/etc/systemd/system/autohotspot.service ${ROOTFS_DIR}/etc/systemd/system/autohotspot.service
+install -m 644 files/etc/wpa_supplicant/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
+install -m 644 files/usr/bin/autohotspotN ${ROOTFS_DIR}/usr/bin/autohotspotN
+
+on_chroot << EOF
 
 # hostapd and dnsmasq will be controlled by the autohotspot service
 systemctl unmask hostapd
@@ -15,3 +17,5 @@ systemctl disable dnsmasq
 
 chmod +x /usr/bin/autohotspotN
 systemctl enable autohotspot.service
+
+EOF
