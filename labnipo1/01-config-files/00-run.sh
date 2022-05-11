ccf629581e2539f26e6567d98faee514a8d7d7db
Nicolas Poirier <nicolas2.poirier@laposte.net>
2022-05-11 17:50:31 +0200

    Update 00-run.sh

--- labnipo1/01-config-files/00-run.sh
+++ labnipo1/01-config-files/00-run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 
-install -m 644 files/etc/dhcpcd.conf ${ROOTFS_DIR}/etc/dhcpcd.conf
-install -m 644 files/etc/dnsmasq.conf ${ROOTFS_DIR}/etc/dnsmasq.conf
-install -m 644 files/etc/sysctl.conf ${ROOTFS_DIR}/etc/sysctl.conf
-install -m 644 files/etc/sudoers.d/labnipo ${ROOTFS_DIR}/etc/sudoers.d/labnipo
-install -m 644 files/etc/systemd/system/autohotspot.service ${ROOTFS_DIR}/etc/systemd/system/autohotspot.service
-install -m 644 files/etc/wpa_supplicant/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
-install -m 644 files/usr/bin/autohotspotN ${ROOTFS_DIR}/usr/bin/autohotspotN
+install -m 644 files/etc/dhcpcd.conf "${ROOTFS_DIR}/etc/dhcpcd.conf"
+install -m 644 files/etc/dnsmasq.conf "${ROOTFS_DIR}/etc/dnsmasq.conf"
+install -m 644 files/etc/sysctl.conf "${ROOTFS_DIR}/etc/sysctl.conf"
+install -m 644 files/etc/sudoers.d/labnipo "${ROOTFS_DIR}/etc/sudoers.d/labnipo"
+install -m 644 files/etc/systemd/system/autohotspot.service "${ROOTFS_DIR}/etc/systemd/system/autohotspot.service"
+install -m 644 files/etc/wpa_supplicant/wpa_supplicant.conf "${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf"
+install -m 644 files/usr/bin/autohotspotN "${ROOTFS_DIR}/usr/bin/autohotspotN"
 
 on_chroot << EOF
 
